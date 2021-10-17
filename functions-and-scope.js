7038360798bd4af6e5c275d33065677ad19342a1
dennisolislagers <dennis.olislagers@gmail.com>
2021-10-17 11:50:07 +0200

    aanpassingen

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -3,7 +3,10 @@
 // Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
 
 const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+
 console.log(grades);
+console.log(grades.length);
+
 /* Opdracht  1: Cum Laude */
 
 /* 1a: Script schrijven  */
@@ -14,14 +17,18 @@ console.log(grades);
 // * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden?
 // Log het antwoord in de terminal.
 
-for (let i = 0; i < grades.length; i++) {
-    if ((grades[i] >= 8) === true) {
-        console.log(i++);
-    }
-
-}
-console.log(grades);
 // ---- Verwachte uitkomst: 6
+
+    let count = 0;
+    for (let i = 0; i < grades.length; i++) {
+        if  (grades[i] >= 8)
+        {
+           count++
+        }
+    }
+const outcome = count;
+    console.log(outcome);
+
 
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
@@ -29,17 +36,28 @@ console.log(grades);
 // Zorg ervoor dat jouw functie ook werkt als we een andere array met eindcijfers willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
 // Log het antwoord in de terminal.
 
-function cumLaude(grades) {
-    const highGrades = grades >= 8;
-    return highGrades;
-}
-console.log(cumLaude);
-
 // ---- Verwachte uitkomsten:
 // cumLaude(grades) geeft 6
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
+function cumLaude(grades) {
+    let count = 0;
+    for (let i = 0; i < grades.length; i++) {
+        if (grades[i] >= 8)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+const outcome1 = cumLaude(grades);
+const outcome2 = cumLaude([6, 4, 5]);
+const outcome3 = cumLaude([8, 9, 4, 6, 10]);
+
+console.log(outcome1);
+console.log(outcome2);
+console.log(outcome3);
 
 
 
@@ -48,12 +66,26 @@ console.log(cumLaude);
 /* 2a: Script schrijven  */
 // De studenten-administratie moet ieder blok opnieuw berekenen wat het gemiddelde eindcijfer is, maar we beginnen met de grades array van hierboven.
 // Schrijf de stapjes om dit te kunnen berekenen eerst uit en vraag jezelf de volgende dingen af:
-// * Hoe wordt een gemiddelde berekend?
+// * Hoe wordt een gemiddelde berekend? Eerst het totaal van de array berekenen en dan delen door de lengte van de array
 // * Wat moet ik verzamelen uit de array van cijfers om uiteindelijk een gemiddelde te kunnen berekenen?
 // * Hoe zorgt ik ervoor dat ik alle waardes uit de array kan langslopen, ook als de array wel 100 entries zou bevatten?
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6.642857142857143
+
+    let sum = 0;
+    for (let i = 0; i < grades.length; i++) {
+        let number = grades[i]
+        sum = sum + number
+    }
+    {
+    average = sum / grades.length
+    }
+
+const outcome4 = average;
+console.log(outcome4);
+
+
 
 
 /* 2b: Omschrijven tot een herbruikbare functie */
@@ -65,13 +97,49 @@ console.log(cumLaude);
 // averageGrade(grades) geeft 6.642857142857143
 // averageGrade([6, 4, 5]) geeft xxxx
 // averageGrade([8, 9, 4, 6, 10]) geeft xxxx
+function averageGrade (grades) {
+    let sum = 0;
+    for (let i = 0; i < grades.length; i++) {
+        let number = grades[i]
+        sum = sum + number
+    }
+
+    average = sum / grades.length
+    return average;
+}
+
+const outcome5 = averageGrade(grades);
+const outcome6 = averageGrade([6, 4, 5]);
+const outcome7 = averageGrade([8, 9, 2, 6, 10]);
+
+console.log(outcome5);
+console.log(outcome6);
+console.log(outcome7);
+
 
 
 /* 2c: Afronden op twee decimalen */
 // Zorg ervoor dat het gemiddelde cijfer dat wordt teruggegeven uit de functie netjes wordt afgerond op twee decimalen.
 // Tip: Google is your best friend!
 
+function fixedGrade(grades) {
+    let sum = 0;
+    for (let i = 0; i < grades.length; i++) {
+        let number = grades[i]
+        sum = sum + number
+    }
 
+    average = sum / grades.length
+    return average.toFixed(2);
+}
+
+const outcome8 = fixedGrade(grades);
+const outcome9 = fixedGrade([6, 4, 5]);
+const outcome10 = fixedGrade([8, 9, 2, 6, 10]);
+
+console.log(outcome8);
+console.log(outcome9);
+console.log(outcome10);
 
 
 /* Bonusopdracht: hoogste cijfer */
@@ -83,6 +151,14 @@ console.log(cumLaude);
 // * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan opslaan?
 // Log het antwoord in de terminal.
 
+let highestGrades = 0;
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] > highestGrades) {
+        highestGrades = grades [i]
+    }
+}
+const outcome11 = highestGrades
+console.log(outcome11)
 // ---- Verwachte uitkomst: 9
 
 
@@ -95,3 +171,20 @@ console.log(cumLaude);
 // highestGrade(grades) geeft 9
 // highestGrade([6, 4, 5]) geeft 6
 // highestGrade([8, 9, 4, 6, 10]) geeft 10
+
+function highestGrade(grades) {
+    let highestGrades = 0;
+    for (let i = 0; i < grades.length; i++) {
+        if (grades[i] > highestGrades) {
+            highestGrades = grades [i]
+        }
+    }
+    return highestGrades;
+}
+const outcome12 = highestGrade(grades);
+const outcome13 = highestGrade([2, 6, 8, 9, 10, 4]);
+const outcome14 = highestGrade([5, 8, 3, 4, 2]);
+
+console.log(outcome12);
+console.log(outcome13);
+console.log(outcome14);
